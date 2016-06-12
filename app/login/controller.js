715553881c61494c35cbfda474fdf51a6518a95c
Ryan Tablada <ryan.tablada@gmail.com>
2016-06-12 09:34:34 -0500

    Start authentication

--- app/login/controller.js
+++ app/login/controller.js
@@ -1,7 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service(),
+
   login({email, password}) {
-    debugger;
+    this.get('session').authenticate('authenticator:planner', email, password)
+      .catch(() => {
+        window.alert('There was an error');
+      });
   },
 });
